6781031bb08b4ff6bbc5b81801b4e45b5dd76563
Ahadi-mu <ahadimurinda@gmail.com>
2026-05-07 12:15:47 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('WELCOME TO TECHNOLOGY WORLD');
+}) 
+
+app.get('/contact', (req, res) => {
+  res.send('WELCOME TO CONTACT US PAGE');
+}) 
+
+
+
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
